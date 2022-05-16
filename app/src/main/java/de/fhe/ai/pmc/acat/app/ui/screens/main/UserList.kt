96f4f8af8cc347227bc3c00dbd997f2e1c6b1965
Steffen Avemarg <steffen.avemarg@fh-erfurt.de>
2022-05-16 22:31:47 +0200

    Removed unused imports

--- app/src/main/java/de/fhe/ai/pmc/acat/app/ui/screens/main/UserList.kt
+++ app/src/main/java/de/fhe/ai/pmc/acat/app/ui/screens/main/UserList.kt
@@ -3,10 +3,8 @@ package de.fhe.ai.pmc.acat.app.ui.screens.main
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import de.fhe.ai.pmc.acat.app.ui.screens.util.UserListPreviewParameterProvider
